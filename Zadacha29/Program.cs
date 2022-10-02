f3854b54152843ae144b8248a89b22d57b2051b3
AmarrPrime <zloybirik@gmail.com>
2022-10-02 18:47:32 +0300

    Task 29 done

---
+++ Zadacha29/Program.cs
@@ -0,0 +1,13 @@
+﻿//Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+Console.Write("Введите числа через запятую ");
+
+int [] array = new int[8];
+string arraytoenter = Console.ReadLine() ?? "";
+string[] mass = arraytoenter.Split(",");
+for (int i = 0; i < mass.Length; i++)
+{
+    array[i] = int.Parse(mass[i]);
+}
+
+Console.WriteLine("[{0}]", string.Join(", ", array));
