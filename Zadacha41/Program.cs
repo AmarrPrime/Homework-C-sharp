027fcabe7308b59f4ef8b9fb3e9e919d49745001
AmarrPrime <zloybirik@gmail.com>
2022-10-05 23:55:04 +0300

    task41 done

---
+++ Zadacha41/Program.cs
@@ -0,0 +1,17 @@
+﻿//Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+Console.WriteLine("Сколько чисел хотите проверить? ");
+int numbersEntered = int.Parse(Console.ReadLine() ?? "");
+int count = 0;
+
+for (int i = 0; i < numbersEntered; i++)
+{
+    int temp = int.Parse(Console.ReadLine() ?? "");
+
+    if (temp > 0)
+    {
+        count++;
+    }
+}
+
+Console.WriteLine($"Вы ввели {count} положительных чисел.");
