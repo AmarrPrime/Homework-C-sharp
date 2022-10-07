84603839d96942d6f3ff3c976c3984af98f53642
AmarrPrime <zloybirik@gmail.com>
2022-10-08 01:30:13 +0300

    Task50 done

---
+++ Zadacha50/Program.cs
@@ -0,0 +1,76 @@
+﻿/*Напишите программу, которая на вход принимает число и возвращает позицию (i, j) этого элемента или же указание, что такого элемента нет.
+
+Например, задан массив:
+
+1 4 7 2
+
+5 9 2 3
+
+8 4 2 4
+
+17 -> такого числа в массиве нет */
+
+
+int GetNumber(string message)
+{
+    Console.WriteLine(message);
+    int number = int.Parse(Console.ReadLine() ?? "");
+    return number;
+}
+
+int[,] InitMatrix(int m, int n)
+{
+    int[,] matrix = new int[m, n];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(1, 10);
+        }
+    }
+
+    return matrix;
+}
+
+
+
+void FindNumberPosition(int k, int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (k == matrix[i, j])
+            {
+                Console.WriteLine($"Такое число есть в массиве на позиции {i},{j}. ");
+                return;
+            }
+        }
+
+    }
+
+    Console.WriteLine("Число не найдено.");
+
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int m = GetNumber("Введите число m");
+int n = GetNumber("Введите число n");
+int[,] matrix = InitMatrix(m, n);
+int k = GetNumber("Какое число будем искать?");
+Console.WriteLine("Матрица:");
+PrintMatrix(matrix);
+FindNumberPosition(k, matrix);
